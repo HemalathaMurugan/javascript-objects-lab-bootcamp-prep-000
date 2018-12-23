f0d65282e1ff74cb8a2f7532f2df6176ecdc6de4
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-23 21:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 
 function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({},object, {key:value});
- //object[key]=value;
-  }
+   }
   
   function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
     object[key]=value;
