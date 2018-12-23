8820ab084f36fb09d6b7541da34dbb6d7f94b732
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-23 21:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function objects(){
+//function objects(){
   var recipes={"key":"value"};
-} 
+//} 
 
 function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({recipes}, {key:value});
